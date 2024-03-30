bb2e0cf1394298309fc34f64471d83fc1ffb1a00
adamjkelley01 <adamjkelley01@gmail.com>
2024-03-29 20:38:03 -0600

    All done yay

--- Adam_Kelley_Mission_11/Controllers/HomeController.cs
+++ Adam_Kelley_Mission_11/Controllers/HomeController.cs
@@ -8,29 +8,37 @@ namespace Adam_Kelley_Mission_11.Controllers
     public class HomeController : Controller
     {
         private IBookStoreRepository _repo;
-        public HomeController(IBookStoreRepository temp) 
+
+        // Constructor injection of IBookStoreRepository
+        public HomeController(IBookStoreRepository temp)
         {
             _repo = temp;
         }
+
+        // Action method for rendering the home page with pagination
         public IActionResult Index(int pageNum)
         {
-            int pageSize = 3;
+            int pageSize = 10; // Number of items per page
 
-            var blah = new BooksListViewModel
+            // Retrieving books from the repository, applying pagination
+            var viewModel = new BooksListViewModel
             {
                 Books = _repo.Books
-                .OrderBy(x => x.Title)
-                .Skip((pageNum - 1) * pageSize)
-                .Take(pageSize),
+                    .OrderBy(x => x.Title) // Order books by title
+                    .Skip((pageNum - 1) * pageSize) // Skip pages based on current page number
+                    .Take(pageSize), // Take only the required number of items per page
 
+                // Pagination information
                 PaginationInfo = new PaginationInfo
                 {
-                    CurrentPage = pageNum,
-                    ItemsPerPage = pageSize,
-                    TotalItems = _repo.Books.Count()
+                    CurrentPage = pageNum, // Current page number
+                    ItemsPerPage = pageSize, // Number of items per page
+                    TotalItems = _repo.Books.Count() // Total number of items in the repository
                 }
             };
-            return View(blah);
+
+            // Render the view with the constructed view model
+            return View(viewModel);
         }
     }
 }
--- Adam_Kelley_Mission_11/Infrastructure/PaginationTagHelper.cs
+++ Adam_Kelley_Mission_11/Infrastructure/PaginationTagHelper.cs
@@ -7,39 +7,69 @@ using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace Adam_Kelley_Mission_11.Infrastructure
 {
-    [HtmlTargetElement("div", Attributes ="page-model")]
+    // This TagHelper targets <div> elements with the "page-model" attribute
+    [HtmlTargetElement("div", Attributes = "page-model")]
     public class PaginationTagHelper : TagHelper
     {
         private IUrlHelperFactory urlHelperFactory;
 
-        public PaginationTagHelper (IUrlHelperFactory temp)
+        // Constructor for PaginationTagHelper
+        public PaginationTagHelper(IUrlHelperFactory temp)
         {
             urlHelperFactory = temp;
         }
 
+        // Properties
         [ViewContext]
         [HtmlAttributeNotBound]
         public ViewContext? ViewContext { get; set; }
+
         public string? PageAction { get; set; }
+
         public PaginationInfo PageModel { get; set; }
+
+        public bool PageClassesEnabled { get; set; } = false;
+
+        public string PageClass { get; set; } = String.Empty;
+
+        public string PageClassNormal { get; set; } = String.Empty;
+
+        public string PageClassSelected { get; set; } = String.Empty;
+
+        // Process method for generating pagination HTML
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             if (ViewContext != null && PageModel != null)
             {
+                // Get the URL helper
                 IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
 
+                // Create a new <div> tag
                 TagBuilder result = new TagBuilder("div");
 
+                // Loop through each page and create an <a> tag for it
                 for (int i = 1; i <= PageModel.TotalNumPages; i++)
                 {
                     TagBuilder tag = new TagBuilder("a");
 
+                    // Set the href attribute of the <a> tag
                     tag.Attributes["href"] = urlHelper.Action(PageAction, new { pageNum = i });
+
+                    // Add CSS classes based on PageClassesEnabled and the current page
+                    if (PageClassesEnabled)
+                    {
+                        tag.AddCssClass(PageClass);
+                        tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                    }
+
+                    // Set the inner HTML of the <a> tag to the page number
                     tag.InnerHtml.Append(i.ToString());
 
+                    // Append the <a> tag to the result <div>
                     result.InnerHtml.AppendHtml(tag);
                 }
 
+                // Append the content of the result <div> to the output
                 output.Content.AppendHtml(result.InnerHtml);
             }
         }
